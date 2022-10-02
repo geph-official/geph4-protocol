ea24d0780559d2fb12107772841ea66e1d93a87c
nullchinchilla <nullchinchilla@pm.me>
2022-10-01 21:13:18 -0500

    fi

--- src/tunnel/getsess.rs
+++ src/tunnel/getsess.rs
@@ -21,7 +21,6 @@ pub fn sosistab_udp(
     reset_interval: Duration,
     stats_gatherer: Arc<sosistab::StatsGatherer>,
 ) -> sosistab::ClientConfig {
-    log::debug!("UDP!");
     sosistab::ClientConfig::new(
         sosistab::Protocol::DirectUdp,
         server_addr,
@@ -42,7 +41,6 @@ pub fn sosistab_tcp(
     reset_interval: Duration,
     stats_gatherer: Arc<sosistab::StatsGatherer>,
 ) -> sosistab::ClientConfig {
-    log::debug!("TCP!");
     sosistab::ClientConfig::new(
         sosistab::Protocol::DirectTcp,
         server_addr,
