21d64ab00b881df1753f0a1b35121757920735f5
nullchinchilla <nullchinchilla@pm.me>
2022-10-07 20:39:43 -0500

    force vendoring

---
+++ src/bin/bridge-exit-test.rs
@@ -0,0 +1,17 @@
+use async_net::SocketAddr;
+use geph4_protocol::bridge_exit::{BridgeExitClient, BridgeExitTransport, RawProtocol};
+
+fn main() -> anyhow::Result<()> {
+    smol::future::block_on(async {
+        let bridge_secret = blake3::hash(std::env::var("BRIDGE_SECRET").unwrap().as_bytes());
+        let exit_addr: SocketAddr = "155.138.252.132:28080".parse().unwrap();
+        let transport = BridgeExitTransport::new(*bridge_secret.as_bytes(), exit_addr);
+        let client = BridgeExitClient(transport);
+        dbg!(
+            client
+                .advertise_raw(RawProtocol::Udp, exit_addr, "fake-lol".into())
+                .await?
+        );
+        Ok(())
+    })
+}
