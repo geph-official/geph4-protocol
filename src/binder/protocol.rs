a465dd74502177cc01e78f0903911b0564e0b931
antidmg <antidmg@proton.me>
2023-04-20 16:30:04 -0500

    serde rename sosistab2_pk
    Co-authored-by: sadministrator <marcothewebdev@gmail.com>

--- src/binder/protocol.rs
+++ src/binder/protocol.rs
@@ -342,6 +342,7 @@ pub struct ExitDescriptor {
     pub country_code: SmolStr,
     pub city_code: SmolStr,
     pub direct_routes: Vec<BridgeDescriptor>,
+    #[serde(rename(serialize = "sosistab2_pk", deserialize = "sosistab2_pk"))]
     pub legacy_direct_sosistab_pk: x25519_dalek::PublicKey,
     pub allowed_levels: Vec<Level>,
     pub load: f64,
